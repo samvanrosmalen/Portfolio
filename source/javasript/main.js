d9c905532ca38e7bec9967ec4ff22434e1ee41e3
samvanrosmalen <samvanrosmalen@live.nl>
2021-04-29 15:09:49 +0200

    custom cursor maken

--- source/javasript/main.js
+++ source/javasript/main.js
@@ -1,5 +1,14 @@
+
+// svg logo
 const logo = document.querySelectorAll("#logosvg path");
 
 for(let i = 0; i<logo.length; i++){
     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
 }
+
+// custom cursor
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px")
+})
