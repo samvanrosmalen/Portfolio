7b71b07393fb4aa804d9b0e955039c54ceff3631
samvanrosmalen <samvanrosmalen@live.nl>
2021-04-29 15:35:59 +0200

    custom cursor gemaakt

--- source/javasript/main.js
+++ source/javasript/main.js
@@ -10,5 +10,13 @@ for(let i = 0; i<logo.length; i++){
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px")
+    cursor.setAttribute("style", "top: "+(e.pageY - 15)+"px; left: "+(e.pageX - 15)+"px")
+})
+
+document.addEventListener('click', () =>{
+    cursor.classList.add("expand")
+
+    setTimeout(()=>{
+        cursor.classList.remove("expand");
+    }, 500)
 })
